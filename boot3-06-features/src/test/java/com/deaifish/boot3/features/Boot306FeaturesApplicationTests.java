6e6babf928adf5c3f328d1cf7630146c9c0396e2
DEAIFISH <1277676309@qq.com>
2023-08-13 15:09:10 +0800

    2023/08/13

---
+++ boot3-06-features/src/test/java/com/deaifish/boot3/features/Boot306FeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deaifish.boot3.features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot306FeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
