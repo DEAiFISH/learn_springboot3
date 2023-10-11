fd329ba77151d8e58dbf32966ac41b34eacc4643
DEAIFISH <1277676309@qq.com>
2023-10-11 18:00:31 +0800

    2023/10/11

--- boot3-07-core/src/main/java/org/deaifish/boot3/core/Main.java
+++ boot3-07-core/src/main/java/org/deaifish/boot3/core/Main.java
@@ -1,10 +1,14 @@
 package org.deaifish.boot3.core;
 
+import com.deaifish.boot3.robot.starter.robot.anotation.EnableRobot;
+import com.deaifish.boot3.robot.starter.robot.configration.RobotAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@EnableRobot
 public class Main {
     public static void main(String[] args) {
         new SpringApplication(Main.class).run(args);
